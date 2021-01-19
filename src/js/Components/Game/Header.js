138fdd4c07165cb8b27e6986ee4595562af99ba0
Dudu Popkhadze <dudupopkhadze@gmail.com>
2021-01-20 01:55:11 +0400

    back to home func

--- src/js/Components/Game/Header.js
+++ src/js/Components/Game/Header.js
@@ -1,8 +1,24 @@
 import React from 'react'
 
 const Header = ({ visitorTeam, homeTeam, game }) => {
+  const goToHome = () =>
+    (window.location.href = window.location.href.substr(
+      0,
+      window.location.href.indexOf('game.html')
+    ))
+
   return (
     <div className='Game-Header'>
+      <div className='Game-Header-backContainer'>
+        <img
+          onClick={goToHome}
+          src='./static/left-arrow.png'
+          className='Game-Header-backContainer-icon'
+        />
+        <div onClick={goToHome} className='Game-Header-backContainer-text'>
+          Back To Home
+        </div>
+      </div>
       <div className='Game-Header-avatarsContainer'>
         <img
           className='Game-Header-avatarsContainer-avatar'
